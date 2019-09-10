49fe83a4b8452bd9a8328e62329e8c135090cbaf
Kishor Naik <kishor.naik011.net@gmail.com>
2019-09-10 17:28:04 +0530

    #Code Commit

---
+++ Sol_Custom_Error_Page/Sol_Custom_Error_Page/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sol_Custom_Error_Page.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
