127d5bc46c5e5044e74cc8baf0c7d052775e45bd
appvissta <appvissta@gmail.com>
2019-12-05 12:52:36 +0530

    Code Commit

---
+++ Sol_Custom_Error_Page/Sol_Custom_Error_Page/Controllers/StatusCodeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Custom_Error_Page.Controllers
+{
+    public class StatusCodeController : Controller
+    {
+        [HttpGet("/StatusCode/{statusCode}")]
+        public IActionResult Index(int statusCode)
+        {
+
+            //HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            return View(statusCode);
+        }
+    }
+}
